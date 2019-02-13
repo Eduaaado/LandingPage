15336ff2826e32d1b84206d8b995204efc3f938f
Eduardo Rodrigues <44787022+Eduaaado@users.noreply.github.com>
2019-02-13 19:41:33 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+//NAVIGATION BAR
+
 $(window).on('scroll', function() {
 	if ($(window).scrollTop()) {
 		$('nav').addClass('black')
 	} else {
 		$('nav').removeClass('black');
 	}
-});
 
-$(window).scroll(function() {
+//PARALLAX
+
 	var scrollTop = $(this).scrollTop();
 
 	$('.sec-bg').css('top', (scrollTop * 0.8) + 'px')
-})
+
+});
+
+//TEXT REVEAL
+
+$(document).ready(function() {
+	$(window).scroll(function() {
+		var y = $(this).scrollTop();
+
+		if (y > 300) {
+			$('h1').addClass('textanimation')
+			$('div').addClass('blockanimation')
+		}
+	});
+});
